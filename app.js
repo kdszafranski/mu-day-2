0ed3e94a463915c440ff5c1c0b02c7feda60c803
Kris Szafranski <kdszafranski@gmail.com>
2016-04-27 11:29:02 -0500

    made change

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var text = 'Apple, Banana, Kiwi';
 var split = text.substring(1);
-// var letter = string.charAt(999);
-console.log(split);
+var letter = string.charAt(999);
+
+
 console.log('time for the speaker!');
